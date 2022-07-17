da170b74594867e49659fe69f9654ba8b0c0734d
monika <monika>
2022-07-17 16:53:45 -0700

    input fild done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 
 
 var todoapp = document.getElementById("todo-app");
+var addingNewtodowork=document.getElementById('addingNewtodowork');
+var makelistadd=document.getElementById('makelistadd');
 var todoItem = [
   { id: 1, title: "Make to app", description: "complete it by monday" },
   { id: 2, title: "Play UNO", description: "complete it by monday" },
-  { id: 3, title: "Dance Practice", description: "complete it by monday" },
-  { id: 4, title: "Learn Pttho", description: "complete it by monday" },
-  { id: 5, title: "Make to app", description: "complete it by monday" }
+  { id: 3, title: "Dance Practice", description: "complete it by monday" }
+
 ];
 
 const showAllList = () => {
@@ -17,6 +18,7 @@ const showAllList = () => {
       <h2>${todoItem[i].title}</h2>
       <p>${todoItem[i].description}</p>
     </div>
+    
   </div>`;
 
     htmlString = htmlString + singleString;
@@ -40,3 +42,16 @@ const showAllListWithForeach = () => {
   todoapp.innerHTML = htmlString;
 };
 showAllListWithForeach();
+ 
+
+// this function display the add list filed
+const addListBtn=()=>
+{
+        if (addingNewtodowork.style.display === 'none') {
+            makelistadd.innerText="X";
+            addingNewtodowork.style.display = 'block';
+          } else {
+            makelistadd.innerText="+";
+            addingNewtodowork.style.display = 'none';
+          }
+}
